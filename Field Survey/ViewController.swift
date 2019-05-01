233ae3e2cbcf4e1885ec7f6dbbeaab446eadaa14
[benfrailey] <[bcfdkp@mail.missouri.edu]>
2019-04-30 21:16:42 -0500

    First commit

---
+++ Field Survey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Field Survey
+//
+//  Created by Ben Frailey on 4/30/19.
+//  Copyright © 2019 Ben Frailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
